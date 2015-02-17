718d454a3461a1245886958119d5fc41178faade
felixsusanto <felix.susanto@wds.co>
2015-02-17 15:02:22 +0800

    revision on icon and other stuff

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 $(function () {
+  smoothScroll.init();
   $('[data-toggle="tooltip"]').tooltip();
   $('.pin>a.disabled').click(function(e){
     e.preventDefault();
   });
-  $('.team-icon-container a.disabled').click(function(e){
-    e.preventDefault();
-  });
+  $('body').scrollspy({target: '#navbar-scrollspy'});
+  var pathImg = $('.header-image').data('imgsrc');
+  $('.header-image').css('background-image', 'url('+pathImg+')');
 });
